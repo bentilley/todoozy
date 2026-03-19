7e6d0508e066f9bb5bdda0b43a0f3bfc4af82b01
Ben Tilley <tilleyben@hotmail.com>
2026-03-19 10:52:25 +0000

    fix(lang/sh): escape sequences in " quoted strings

--- src/lang/sh.rs
+++ src/lang/sh.rs
@@ -2,8 +2,8 @@ use super::SyntaxRule;
 
 pub const SH: [SyntaxRule; 3] = [
     SyntaxRule::LineComment(b"#"),
-    SyntaxRule::SkipDelimited(b"\"", b"\""),
-    SyntaxRule::SkipDelimited(b"'", b"'"),
+    SyntaxRule::SkipDelimitedWithEscape(b"\"", b"\"", b'\\'),
+    SyntaxRule::SkipDelimited(b"'", b"'"), // shell single quotes don't support escaping
 ];
 
 #[cfg(test)]
@@ -64,15 +64,97 @@ more='code'
         assert_eq!(todos[0], (7, 7, "this is a real todo".to_string()));
     }
 
-    // TODO #46 (A) 2026-03-16 Fix " string parsing in shell lang
     #[test]
-    #[ignore = "documents parsing limitation: escaped quotes"]
-    fn escaped_quote_in_double_quoted_string_not_handled() {
+    fn escaped_quote_in_double_quoted_string() {
         let parser = crate::lang::Parser::new("TODO", &SH);
         let text = r##"
 msg="hello \"
 # TODO false positive
 world"
+
+# TODO real todo
+"##;
+        let todos = parser.parse_todos(text);
+        assert_eq!(todos.len(), 1);
+        assert_eq!(todos[0].2, "real todo".to_string());
+    }
+
+    #[test]
+    fn escaped_backslash_before_closing_double_quote() {
+        // `\\` escapes the backslash, so the quote ends the string
+        let parser = crate::lang::Parser::new("TODO", &SH);
+        let text = r##"
+msg="hello \\"
+
+# TODO real todo
+"##;
+        let todos = parser.parse_todos(text);
+        assert_eq!(todos.len(), 1);
+        assert_eq!(todos[0].2, "real todo".to_string());
+    }
+
+    #[test]
+    fn triple_backslash_before_double_quote() {
+        // `\\\"` = escaped backslash + escaped quote, string continues
+        let parser = crate::lang::Parser::new("TODO", &SH);
+        let text = r##"
+msg="hello \\\"
+# TODO false positive
+world"
+
+# TODO real todo
+"##;
+        let todos = parser.parse_todos(text);
+        assert_eq!(todos.len(), 1);
+        assert_eq!(todos[0].2, "real todo".to_string());
+    }
+
+    #[test]
+    fn multiple_escaped_double_quotes() {
+        let parser = crate::lang::Parser::new("TODO", &SH);
+        let text = r##"
+msg="\"hello\" \"world\""
+
+# TODO real todo
+"##;
+        let todos = parser.parse_todos(text);
+        assert_eq!(todos.len(), 1);
+        assert_eq!(todos[0].2, "real todo".to_string());
+    }
+
+    #[test]
+    fn escaped_double_quote_at_end_of_string() {
+        let parser = crate::lang::Parser::new("TODO", &SH);
+        let text = r##"
+msg="hello \""
+
+# TODO real todo
+"##;
+        let todos = parser.parse_todos(text);
+        assert_eq!(todos.len(), 1);
+        assert_eq!(todos[0].2, "real todo".to_string());
+    }
+
+    #[test]
+    fn other_escape_sequences_in_double_quoted_string() {
+        // Various escapes shouldn't break parsing
+        let parser = crate::lang::Parser::new("TODO", &SH);
+        let text = r##"
+msg="hello\nworld\t\$var"
+
+# TODO real todo
+"##;
+        let todos = parser.parse_todos(text);
+        assert_eq!(todos.len(), 1);
+        assert_eq!(todos[0].2, "real todo".to_string());
+    }
+
+    #[test]
+    fn escape_sequences_mixed_with_escaped_double_quotes() {
+        // Mix of regular escapes and escaped quotes
+        let parser = crate::lang::Parser::new("TODO", &SH);
+        let text = r##"
+msg="line1\nline2\t\"quoted\"\\done"
 
 # TODO real todo
 "##;
